437fbb7b8c5f21b8532f224500af22240369cdb5
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-14 21:26:14 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,3 @@
-class Post < ActiveRecord::Baseinclude
-
-  validates_with PostValidator
-
-    validates :title, presence: true
-    validates :content, length: { minimum: 100 }
-    validates :summary, length: { maximum: 100}
-    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+class Post < ActiveRecord::Base
+  validates :title, presence: true
 end
