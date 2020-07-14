76d583459645daaff1032edc611e3884307acd3f
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-14 21:32:24 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -3,8 +3,8 @@ class TitleValidator < ActiveModel::Validator
   def validate(record)
     if record.title.blank?
       record.errors[:title] << "A post must have a title."
-    elsif !record.title.include?("Won't Believe") || record.title.include?("Secret") || record.title.include?("Top [number]") || record.title.include?("Guess")
-        record.errors[:title] <<  "Not clickbait-y enough" 
+    elsif !record.title.match?("Won't Believe") || record.title.match?("Secret") || record.title.match?("Top [number]") || record.title.match?("Guess")
+        record.errors[:title] <<  "Not clickbait-y enough"
     end
   end
 end
